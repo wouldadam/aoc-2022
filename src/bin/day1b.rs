3547012e8ff3126a5fb69ffbf93bced7249f0f25
Adam Wood <contact@adamwood.org>
2022-12-01 23:33:52 +0000

    feat: Day 1b

---
+++ src/bin/day1b.rs
@@ -0,0 +1,24 @@
+/// Takes a text file with numeric values representing calories of foods carried by elves.
+/// An empty line indicates that the calories are carried by a new elf.
+/// Prints the sum of the calories carried by the 3 largest calorie carrying single elves.
+fn main() {
+    let input = include_str!("../../assets/day1a.txt");
+
+    // Parse the file into foods for each elf
+    let foods_per_elf = input
+        .split("\n\n")
+        .map(|elf| elf.lines().map(|cal| cal.parse::<u32>().unwrap()));
+
+    // Sum the calories for each elf
+    let mut calories_per_elf = foods_per_elf
+        .map(|foods| foods.sum::<u32>())
+        .collect::<Vec<_>>();
+
+    // Sort the calories
+    calories_per_elf.sort();
+
+    // Sum the largest 3
+    let sum: u32 = calories_per_elf.iter().rev().take(3).sum();
+
+    println!("Sum of largest 3 calories: {}", sum);
+}
