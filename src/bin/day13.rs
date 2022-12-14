5011308ee8f1ad831a3ef2a10479f86da2f3efb1
Adam Wood <contact@adamwood.org>
2022-12-14 16:14:28 +0000

    feat: Day 13a

---
+++ src/bin/day13.rs
@@ -0,0 +1,153 @@
+use std::{cell::RefCell, rc::Rc};
+
+/// Input is pairs of lists that represent a packet.
+/// Pairs are line separated.
+/// Part A:
+/// Compare each pair of packets with the rules:
+/// Both ints: lower should come first.
+/// Both list: Compare each item in list. Left should run out of items first.
+/// One list: convert int to list and treat as above.
+/// Print the some of the indices (1-indexed) of correctly ordered packet pairs.
+
+#[derive(Debug)]
+enum ListItem {
+    Value(u32),
+    List(Vec<RcListItem>),
+}
+
+type RcListItem = Rc<RefCell<ListItem>>;
+
+impl ListItem {
+    fn list() -> RcListItem {
+        Rc::new(RefCell::new(ListItem::List(vec![])))
+    }
+
+    fn value(value: u32) -> RcListItem {
+        Rc::new(RefCell::new(ListItem::Value(value)))
+    }
+
+    fn add(&mut self, item: RcListItem) {
+        match self {
+            ListItem::Value(_) => panic!("Can't add an item to a value"),
+            ListItem::List(ref mut list) => list.push(item),
+        }
+    }
+
+    fn last_list(&mut self) -> RcListItem {
+        match self {
+            ListItem::Value(_) => panic!("List isn't a list"),
+            ListItem::List(ref mut list) => list.last().unwrap().clone(),
+        }
+    }
+
+    fn parse(s: &str) -> RcListItem {
+        let chars = s.chars().collect::<Vec<_>>();
+        let top = ListItem::list();
+        let mut stack = vec![top.clone()];
+
+        let mut pos: usize = 1;
+
+        while pos < chars.len() {
+            let current = stack.last().unwrap().clone();
+            if chars[pos] == '[' {
+                current.borrow_mut().add(ListItem::list());
+                stack.push(current.borrow_mut().last_list());
+
+                pos += 1;
+            } else if chars[pos] == ']' {
+                stack.pop();
+                pos += 1;
+            } else if chars[pos] == ',' {
+                pos += 1;
+            } else {
+                let end = chars[pos + 1..]
+                    .iter()
+                    .position(|c| *c == ',' || *c == ']')
+                    .unwrap()
+                    + pos
+                    + 1;
+
+                let x = &chars[pos..end].iter().collect::<String>();
+                let value = x.parse::<u32>().unwrap();
+
+                current.borrow_mut().add(ListItem::value(value));
+                pos = end;
+            }
+        }
+
+        top
+    }
+
+    #[allow(dead_code)]
+    fn to_str(&self) -> String {
+        match self {
+            ListItem::Value(v) => v.to_string(),
+            ListItem::List(l) => {
+                let mut s = String::from("[");
+                for i in l.iter() {
+                    s += &i.borrow().to_str();
+                    s += ","
+                }
+                s += "]";
+                s
+            }
+        }
+    }
+}
+
+fn ordering(left: RcListItem, right: RcListItem) -> std::cmp::Ordering {
+    match (&*left.borrow(), &*right.borrow()) {
+        (ListItem::Value(lv), ListItem::Value(rv)) => lv.cmp(rv),
+        (ListItem::Value(lv), ListItem::List(_)) => {
+            let wrapper = Rc::new(RefCell::new(ListItem::List(vec![ListItem::value(*lv)])));
+            ordering(wrapper, right.clone())
+        }
+        (ListItem::List(_), ListItem::Value(rv)) => {
+            let wrapper = Rc::new(RefCell::new(ListItem::List(vec![ListItem::value(*rv)])));
+            ordering(left.clone(), wrapper)
+        }
+        (ListItem::List(ll), ListItem::List(rl)) => {
+            for idx in 0..ll.len() {
+                // Right side ran out of items
+                if idx >= rl.len() {
+                    return std::cmp::Ordering::Greater;
+                }
+
+                match ordering(ll[idx].clone(), rl[idx].clone()) {
+                    std::cmp::Ordering::Less => return std::cmp::Ordering::Less,
+                    std::cmp::Ordering::Equal => continue,
+                    std::cmp::Ordering::Greater => return std::cmp::Ordering::Greater,
+                }
+            }
+
+            // Left side ran out of items
+            if ll.len() < rl.len() {
+                return std::cmp::Ordering::Less;
+            }
+
+            // Inconclusive
+            std::cmp::Ordering::Equal
+        }
+    }
+}
+
+fn main() {
+    let input = include_str!("../../assets/day13.txt");
+
+    let packet_pairs = input
+        .split("\n\n")
+        .map(|pair| pair.lines().collect::<Vec<_>>())
+        .map(|pair| (ListItem::parse(pair[0]), ListItem::parse(pair[1])))
+        .collect::<Vec<_>>();
+
+    // Sum the indices of correctly ordered packets
+    let indices_sum = packet_pairs
+        .iter()
+        .enumerate()
+        .map(|(idx, pair)| (idx + 1, ordering(pair.0.clone(), pair.1.clone())))
+        .filter(|pair| pair.1 != std::cmp::Ordering::Greater)
+        .map(|pair| pair.0)
+        .sum::<usize>();
+
+    println!("Indices sum of correctly ordered packets {}", indices_sum);
+}
