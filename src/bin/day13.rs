a247cb6371f2ad7bc4a53d4187c14e225c96fb86
Adam Wood <contact@adamwood.org>
2022-12-14 16:47:14 +0000

    feat: Day 13b

--- src/bin/day13.rs
+++ src/bin/day13.rs
@@ -2,24 +2,27 @@ use std::{cell::RefCell, rc::Rc};
 
 /// Input is pairs of lists that represent a packet.
 /// Pairs are line separated.
-/// Part A:
 /// Compare each pair of packets with the rules:
 /// Both ints: lower should come first.
 /// Both list: Compare each item in list. Left should run out of items first.
 /// One list: convert int to list and treat as above.
+/// Part A:
 /// Print the some of the indices (1-indexed) of correctly ordered packet pairs.
+/// Part B:
+/// Sort the packets and insert two extras [[2]] [[6]].
+/// Print the sort indices of the two extra packets multiplied together.
 
 #[derive(Debug)]
 enum ListItem {
     Value(u32),
-    List(Vec<RcListItem>),
+    List(Vec<RcListItem>, bool),
 }
 
 type RcListItem = Rc<RefCell<ListItem>>;
 
 impl ListItem {
     fn list() -> RcListItem {
-        Rc::new(RefCell::new(ListItem::List(vec![])))
+        Rc::new(RefCell::new(ListItem::List(vec![], false)))
     }
 
     fn value(value: u32) -> RcListItem {
@@ -29,20 +32,27 @@ impl ListItem {
     fn add(&mut self, item: RcListItem) {
         match self {
             ListItem::Value(_) => panic!("Can't add an item to a value"),
-            ListItem::List(ref mut list) => list.push(item),
+            ListItem::List(ref mut list, _) => list.push(item),
         }
     }
 
     fn last_list(&mut self) -> RcListItem {
         match self {
             ListItem::Value(_) => panic!("List isn't a list"),
-            ListItem::List(ref mut list) => list.last().unwrap().clone(),
+            ListItem::List(ref mut list, _) => list.last().unwrap().clone(),
         }
     }
 
-    fn parse(s: &str) -> RcListItem {
+    fn is_divider(&self) -> bool {
+        match self {
+            ListItem::Value(_) => false,
+            ListItem::List(_, is_divider) => *is_divider,
+        }
+    }
+
+    fn parse(s: &str, is_divider: bool) -> RcListItem {
         let chars = s.chars().collect::<Vec<_>>();
-        let top = ListItem::list();
+        let top = Rc::new(RefCell::new(ListItem::List(vec![], is_divider)));
         let mut stack = vec![top.clone()];
 
         let mut pos: usize = 1;
@@ -82,7 +92,7 @@ impl ListItem {
     fn to_str(&self) -> String {
         match self {
             ListItem::Value(v) => v.to_string(),
-            ListItem::List(l) => {
+            ListItem::List(l, _) => {
                 let mut s = String::from("[");
                 for i in l.iter() {
                     s += &i.borrow().to_str();
@@ -98,15 +108,21 @@ impl ListItem {
 fn ordering(left: RcListItem, right: RcListItem) -> std::cmp::Ordering {
     match (&*left.borrow(), &*right.borrow()) {
         (ListItem::Value(lv), ListItem::Value(rv)) => lv.cmp(rv),
-        (ListItem::Value(lv), ListItem::List(_)) => {
-            let wrapper = Rc::new(RefCell::new(ListItem::List(vec![ListItem::value(*lv)])));
+        (ListItem::Value(lv), ListItem::List(_, _)) => {
+            let wrapper = Rc::new(RefCell::new(ListItem::List(
+                vec![ListItem::value(*lv)],
+                false,
+            )));
             ordering(wrapper, right.clone())
         }
-        (ListItem::List(_), ListItem::Value(rv)) => {
-            let wrapper = Rc::new(RefCell::new(ListItem::List(vec![ListItem::value(*rv)])));
+        (ListItem::List(_, _), ListItem::Value(rv)) => {
+            let wrapper = Rc::new(RefCell::new(ListItem::List(
+                vec![ListItem::value(*rv)],
+                false,
+            )));
             ordering(left.clone(), wrapper)
         }
-        (ListItem::List(ll), ListItem::List(rl)) => {
+        (ListItem::List(ll, _), ListItem::List(rl, _)) => {
             for idx in 0..ll.len() {
                 // Right side ran out of items
                 if idx >= rl.len() {
@@ -134,13 +150,18 @@ fn ordering(left: RcListItem, right: RcListItem) -> std::cmp::Ordering {
 fn main() {
     let input = include_str!("../../assets/day13.txt");
 
+    // Sum the indices of correctly ordered packet pairs
     let packet_pairs = input
         .split("\n\n")
         .map(|pair| pair.lines().collect::<Vec<_>>())
-        .map(|pair| (ListItem::parse(pair[0]), ListItem::parse(pair[1])))
+        .map(|pair| {
+            (
+                ListItem::parse(pair[0], false),
+                ListItem::parse(pair[1], false),
+            )
+        })
         .collect::<Vec<_>>();
 
-    // Sum the indices of correctly ordered packets
     let indices_sum = packet_pairs
         .iter()
         .enumerate()
@@ -150,4 +171,27 @@ fn main() {
         .sum::<usize>();
 
     println!("Indices sum of correctly ordered packets {}", indices_sum);
+
+    // Sort the packets and add dividers
+    let div_a = ListItem::parse("[[2]]", true);
+    let div_b = ListItem::parse("[[6]]", true);
+
+    let mut packets = packet_pairs
+        .iter()
+        .flat_map(|pair| [pair.0.clone(), pair.1.clone()])
+        .collect::<Vec<_>>();
+    packets.push(div_a);
+    packets.push(div_b);
+
+    packets.sort_by(|a, b| ordering(a.clone(), b.clone()));
+
+    let divider_sum = packets
+        .iter()
+        .enumerate()
+        .map(|p| (p.0 + 1, p.1))
+        .filter(|p| p.1.borrow().is_divider())
+        .map(|p| p.0)
+        .product::<usize>();
+
+    println!("Divider indices sum {}", divider_sum);
 }
