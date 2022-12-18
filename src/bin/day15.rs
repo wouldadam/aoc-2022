af0004f3173166823d427e8ee6128cafeaa37a7f
Adam Wood <contact@adamwood.org>
2022-12-18 21:52:05 +0000

    perf: Faster day 15a

--- src/bin/day15.rs
+++ src/bin/day15.rs
@@ -1,4 +1,4 @@
-use std::collections::HashMap;
+use std::collections::HashSet;
 
 use regex::Regex;
 
@@ -8,49 +8,43 @@ use regex::Regex;
 /// Part A:
 /// The target beacon has not been found by any of the sensors.
 /// Count all the positions on the y=2000000 where a beacon could not be (covered by a sensor).
+/// Part B:
+/// A beacon is not detected by any sensor. It must have coords >= 0, <= 4000000.
+/// Find its tuning frequency by multiplying its x coord by 4000000 and then adding its y coord.
+/// There is only one possible position for the beacon.
 
 type Pos = (i64, i64);
-type ReadingVec = Vec<Reading>;
 
-#[derive(Clone, Copy)]
-struct Reading {
-    sensor: Pos,
-    beacon: Pos,
-    sb_dist: i64,
-}
-
-#[derive(Clone, Copy)]
-enum Cell {
-    Empty,
-    Sensor(Reading),
-    Beacon,
-}
-
+/// The grid of all sensors and beacons
 struct Grid {
-    data: HashMap<Pos, Cell>,
+    sensors: HashSet<Pos>,
+    beacons: HashSet<Pos>,
+    areas: Vec<Area>,
     min: Pos,
     max: Pos,
 }
 
 impl Grid {
-    fn new(min: Pos, max: Pos) -> Self {
-        Grid {
-            data: HashMap::new(),
-            min,
-            max,
-        }
+    fn has_sensor(&self, pos: &Pos) -> bool {
+        self.sensors.contains(pos)
     }
 
-    fn at(&self, pos: Pos) -> Cell {
-        if self.data.contains_key(&pos) {
-            return self.data[&pos];
-        }
-
-        Cell::Empty
+    fn has_beacon(&self, pos: &Pos) -> bool {
+        self.beacons.contains(pos)
     }
 
-    fn set(&mut self, pos: Pos, val: Cell) {
-        self.data.insert(pos, val);
+    fn has_item(&self, pos: &Pos) -> bool {
+        self.has_sensor(pos) || self.has_beacon(pos)
+    }
+
+    fn in_area(&self, pos: &Pos) -> bool {
+        for area in &self.areas {
+            if area.in_area(pos) {
+                return true;
+            }
+        }
+
+        false
     }
 
     #[allow(dead_code)]
@@ -59,89 +53,118 @@ impl Grid {
 
         for y in self.min.1..self.max.1 {
             for x in self.min.0..self.max.0 {
-                let empty = Cell::Empty;
-                let cell = self.data.get(&(x, y)).unwrap_or(&empty);
-                match cell {
-                    Cell::Empty => print!("."),
-                    Cell::Sensor(_) => print!("s"),
-                    Cell::Beacon => print!("b"),
+                if self.has_beacon(&(x, y)) {
+                    print!("b");
+                } else if self.has_sensor(&(x, y)) {
+                    print!("s");
+                } else {
+                    print!(".")
                 }
             }
 
             println!();
         }
     }
+
+    fn from(input: &str) -> Self {
+        let line_regex = Regex::new(
+            r"^Sensor at x=(-?\d*), y=(-?\d*): closest beacon is at x=(-?\d*), y=(-?\d*)$",
+        )
+        .unwrap();
+
+        let mut sensors = HashSet::new();
+        let mut beacons = HashSet::new();
+        let mut areas = Vec::new();
+
+        let mut min = (i64::MAX, i64::MAX);
+        let mut max = (i64::MIN, i64::MIN);
+        for line in input.lines() {
+            let caps = line_regex.captures(line).unwrap();
+
+            let sensor = (
+                caps[1].parse::<i64>().unwrap(),
+                caps[2].parse::<i64>().unwrap(),
+            );
+            sensors.insert(sensor);
+
+            let beacon = (
+                caps[3].parse::<i64>().unwrap(),
+                caps[4].parse::<i64>().unwrap(),
+            );
+            beacons.insert(beacon);
+
+            let area = Area::from(&sensor, &beacon);
+
+            min.0 = min.0.min(area.left());
+            min.1 = min.1.min(area.top());
+
+            max.0 = max.0.max(area.bottom());
+            max.1 = max.1.max(area.right());
+
+            areas.push(area);
+        }
+
+        Grid {
+            sensors,
+            beacons,
+            areas,
+            min,
+            max,
+        }
+    }
+}
+
+/// An area covered by a sensor
+struct Area {
+    pos: Pos,
+    range: i64,
+}
+
+impl Area {
+    fn from(sensor: &Pos, beacon: &Pos) -> Self {
+        let range = m_dist(sensor, beacon);
+
+        Area {
+            pos: *sensor,
+            range,
+        }
+    }
+
+    fn top(&self) -> i64 {
+        self.pos.1 - self.range
+    }
+
+    fn bottom(&self) -> i64 {
+        self.pos.1 + self.range
+    }
+
+    fn left(&self) -> i64 {
+        self.pos.0 - self.range
+    }
+
+    fn right(&self) -> i64 {
+        self.pos.0 + self.range
+    }
+
+    fn in_area(&self, pos: &Pos) -> bool {
+        let dist = m_dist(&self.pos, pos);
+
+        self.range >= dist
+    }
 }
 
 /// Calculate the Manhattan distance between two positions
-fn m_dist(a: Pos, b: Pos) -> i64 {
+fn m_dist(a: &Pos, b: &Pos) -> i64 {
     (a.0 - b.0).abs() + (a.1 - b.1).abs()
 }
 
-fn load_readings(input: &str) -> (ReadingVec, Pos, Pos) {
-    let line_regex =
-        Regex::new(r"^Sensor at x=(-?\d*), y=(-?\d*): closest beacon is at x=(-?\d*), y=(-?\d*)$")
-            .unwrap();
-
-    let mut readings = vec![];
-    let mut min = (i64::MAX, i64::MAX);
-    let mut max = (i64::MIN, i64::MIN);
-    for line in input.lines() {
-        let caps = line_regex.captures(line).unwrap();
-
-        let sensor = (
-            caps[1].parse::<i64>().unwrap(),
-            caps[2].parse::<i64>().unwrap(),
-        );
-        let beacon = (
-            caps[3].parse::<i64>().unwrap(),
-            caps[4].parse::<i64>().unwrap(),
-        );
-
-        let sb_dist = m_dist(sensor, beacon);
-
-        min.0 = min.0.min((sensor.0 - sb_dist).min(beacon.0));
-        min.1 = min.1.min((sensor.1 - sb_dist).min(beacon.1));
-
-        max.0 = max.0.max((sensor.0 + sb_dist).max(beacon.0));
-        max.1 = max.1.max((sensor.1 + sb_dist).max(beacon.1));
-
-        readings.push(Reading {
-            sensor,
-            beacon,
-            sb_dist,
-        });
-    }
-
-    (readings, min, max)
-}
-
-fn build_grid(readings: &ReadingVec, min: Pos, max: Pos) -> Grid {
-    let mut grid = Grid::new(min, max);
-
-    for reading in readings {
-        grid.set(reading.sensor, Cell::Sensor(*reading));
-        grid.set(reading.beacon, Cell::Beacon);
-    }
-
-    grid
-}
-
-fn count_known_empty(grid: &mut Grid, sensors: &ReadingVec, min: Pos, max: Pos, y: i64) -> i64 {
+fn count_known_empty(grid: &mut Grid, y: i64) -> i64 {
     let mut count = 0;
 
-    for x in min.0..max.0 {
-        for reading in sensors {
-            match grid.at((x, y)) {
-                Cell::Empty => {
-                    let sp_dist = m_dist(reading.sensor, (x, y));
-                    if sp_dist <= reading.sb_dist {
-                        count += 1;
-                        break;
-                    }
-                }
-                Cell::Sensor(_) | Cell::Beacon => {}
-            }
+    for x in grid.min.0..grid.max.0 {
+        let pos = (x, y);
+        if !grid.has_item(&pos) && grid.in_area(&pos) {
+            count += 1;
         }
     }
 
@@ -150,10 +173,8 @@ fn count_known_empty(grid: &mut Grid, sensors: &ReadingVec, min: Pos, max: Pos, 
 
 fn main() {
     let input = include_str!("../../assets/day15.txt");
-    let (sensors, min, max) = load_readings(input);
-    let mut grid = build_grid(&sensors, min, max);
+    let mut grid = Grid::from(input);
 
-    let checked = count_known_empty(&mut grid, &sensors, min, max, 2000000);
-
-    println!("Checked {}", checked);
+    let known_empty = count_known_empty(&mut grid, 2000000);
+    println!("Known empty {}", known_empty);
 }
