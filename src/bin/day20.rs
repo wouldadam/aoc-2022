945f245bc300a9f83f17951ab170f059b874a108
Adam Wood <contact@adamwood.org>
2023-01-16 16:28:35 +0000

    feat: Day 20b

--- src/bin/day20.rs
+++ src/bin/day20.rs
@@ -6,24 +6,28 @@ use std::time;
 /// The list wraps.
 /// Part A:
 /// Mix the list then sum the 1000th, 2000th and 3000th numbers after the value 0.
+/// Part B:
+/// Multiply the numbers by 811589153 and mix the list 10 times.
 
-fn mix(list: &mut Vec<(usize, i32)>) {
-    for orig_idx in 0..list.len() {
-        let cur_idx = list
-            .iter()
-            .position(|(idx, _val)| *idx == orig_idx)
-            .unwrap();
-        let (_, val) = list[cur_idx];
+fn mix(list: &mut Vec<(usize, i64)>, count: usize) {
+    for _ in 0..count {
+        for orig_idx in 0..list.len() {
+            let cur_idx = list
+                .iter()
+                .position(|(idx, _val)| *idx == orig_idx)
+                .unwrap();
+            let (_, val) = list[cur_idx];
 
-        let wrap = list.len() as i32 - 1;
-        let mut new_idx = cur_idx as i32 + val;
-        new_idx = ((new_idx % wrap) + wrap) % wrap;
+            let wrap = list.len() as i64 - 1;
+            let mut new_idx = cur_idx as i64 + val;
+            new_idx = ((new_idx % wrap) + wrap) % wrap;
 
-        move_idx(list, cur_idx, new_idx as usize);
+            move_idx(list, cur_idx, new_idx as usize);
+        }
     }
 }
 
-fn move_idx(list: &mut [(usize, i32)], from_idx: usize, to_idx: usize) {
+fn move_idx(list: &mut [(usize, i64)], from_idx: usize, to_idx: usize) {
     if from_idx < to_idx {
         list[from_idx..=to_idx].rotate_left(1);
     } else {
@@ -31,18 +35,7 @@ fn move_idx(list: &mut [(usize, i32)], from_idx: usize, to_idx: usize) {
     }
 }
 
-fn main() {
-    let start = time::SystemTime::now();
-
-    let input = include_str!("../../assets/day20.txt");
-    let mut list = input
-        .lines()
-        .enumerate()
-        .map(|(idx, s)| (idx, s.parse::<i32>().unwrap()))
-        .collect::<Vec<_>>();
-
-    mix(&mut list);
-
+fn sum(list: &[(usize, i64)]) {
     let zero_idx = list.iter().position(|e| e.1 == 0).unwrap();
     let v1 = list[(zero_idx + 1000) % list.len()].1;
     let v2 = list[(zero_idx + 2000) % list.len()].1;
@@ -50,6 +43,29 @@ fn main() {
     let sum = v1 + v2 + v3;
 
     println!("Sum is: {} + {} + {} = {}", v1, v2, v3, sum);
+}
+
+fn main() {
+    let start = time::SystemTime::now();
+
+    let input = include_str!("../../assets/day20.txt");
+    let list = input
+        .lines()
+        .enumerate()
+        .map(|(idx, s)| (idx, s.parse::<i64>().unwrap()))
+        .collect::<Vec<_>>();
+
+    let mut part_a = list.clone();
+    mix(&mut part_a, 1);
+    sum(&part_a);
+
+    let mut part_b = list;
+    part_b = part_b
+        .iter()
+        .map(|(orig_idx, val)| (*orig_idx, *val * 811589153))
+        .collect();
+    mix(&mut part_b, 10);
+    sum(&part_b);
 
     let end = time::SystemTime::now();
     println!("Took {:?}", end.duration_since(start));
